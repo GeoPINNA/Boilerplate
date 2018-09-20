34849a602d90ca6eb35cb69bac73e05472d01680
Shota Shimazu <hornet.live.mf@gmail.com>
2018-09-20 22:44:30 +0900

    Initial Commit

---
+++ Boilerplate/Boilerplate.h
@@ -0,0 +1,19 @@
+//
+//  Boilerplate.h
+//  Boilerplate
+//
+//  Created by Shota Shimazu on 2018/09/20.
+//  Copyright © 2018 Shota Shimazu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Boilerplate.
+FOUNDATION_EXPORT double BoilerplateVersionNumber;
+
+//! Project version string for Boilerplate.
+FOUNDATION_EXPORT const unsigned char BoilerplateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Boilerplate/PublicHeader.h>
+
+
